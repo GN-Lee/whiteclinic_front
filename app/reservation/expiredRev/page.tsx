02bde4654793c0a16cab173dd00771b4717aeae8
GN-Lee <rhkdsud0917@gmail.com>
2025-05-14 17:42:25 +0900

    4일차

--- app/reservation/expiredRev/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ExpiredRev } from "@/components/reservation/ExpiredRev";
-
-const ExpiredRevPage = () => {
-  return <ExpiredRev />;
-};
-
-export default ExpiredRevPage;
--- app/reservation/processRev/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProcessRev } from "@/components/reservation/ProcessRev";
-
-const ProcessRevPage = () => {
-  return <ProcessRev />;
-};
-
-export default ProcessRevPage;
